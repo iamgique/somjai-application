3bd42af9bbb82b1d6844379a8cd427afce1b41bf
iamgique <iamgique@gmail.com>
2020-08-25 14:11:14 +0700

    add uppercase

--- app/src/main/java/com/digitalacademy/somjai/ui/payment/PaymentFragment.kt
+++ app/src/main/java/com/digitalacademy/somjai/ui/payment/PaymentFragment.kt
@@ -3,11 +3,14 @@ package com.digitalacademy.somjai.ui.payment
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -36,6 +39,20 @@ class PaymentFragment : Fragment(), View.OnClickListener {
         val generatePaymentBtn: Button = root.findViewById(R.id.generatePaymentBtn)
         generatePaymentBtn.setOnClickListener(this)
 
+        /*val refTxtVal: EditText = root.findViewById(R.id.refTxt)
+        refTxtVal.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                refTxt.setText(charSequence)
+                //refTxtVal.text.toString().toUpperCase()
+                //refTxt.setText(charSequence.toString().toUpperCase())
+                //refTxt.setSelection(charSequence.toString().length)
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //refTxt.setText(p0)
+            }
+            override fun afterTextChanged(editable: Editable) {}
+        })*/
+
         return root
     }
 
@@ -44,13 +61,13 @@ class PaymentFragment : Fragment(), View.OnClickListener {
         hideKeyboard()
 
         val amount = amountTxt.text.toString()
-        val ref = refTxt.text.toString()
+        val ref = refTxt.text.toString().toUpperCase()
 
         when(view.id) {
-            R.id.generatePaymentBtn -> checkGeneratePayment(amount, ref){ canGenerate ->
-                if(canGenerate) {
+            R.id.generatePaymentBtn -> checkGeneratePayment(amount, ref) { canGenerate ->
+                if (canGenerate) {
                     GenerateQRService.getQRCode(amount, ref) { generateSuccess ->
-                        if(generateSuccess) {
+                        if (generateSuccess) {
                             qrCodeView.setImageBitmap(PaymentDataService.qrPayment)
                             enableSpinner(false)
                         } else {
@@ -68,6 +85,7 @@ class PaymentFragment : Fragment(), View.OnClickListener {
     private fun checkGeneratePayment(amount: String, ref: String, complete: (Boolean) -> Unit) {
         if(amount.isNotEmpty() && ref.isNotEmpty()) {
             convertAmountTo2DecimalString(amount)
+            convertToUpperCase(ref)
             amountTxt.isEnabled = false
             refTxt.isEnabled = false
             generatePaymentBtn.isEnabled = false
@@ -76,14 +94,20 @@ class PaymentFragment : Fragment(), View.OnClickListener {
             complete(true)
         } else {
             println("Make sure user name, email, and password are filled in.")
-            Toast.makeText(activity, "Make sure user name, email, and password are filled in.",
-                Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                activity, "Make sure user name, email, and password are filled in.",
+                Toast.LENGTH_LONG
+            ).show()
             complete(false)
         }
     }
 
     private fun convertAmountTo2DecimalString(amount: String) {
         amountTxt.setText(String.format("%.2f", amount.toFloat()))
+    }
+
+    private fun convertToUpperCase(refTxtParam: String) {
+        refTxt.setText(refTxtParam.toUpperCase())
     }
 
     private fun enableSpinner(enable: Boolean) {
